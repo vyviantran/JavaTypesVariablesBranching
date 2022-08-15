a8bf2074799686c6658eec6ef9d462ef886f207c
vyviantran <107646017+vyviantran@users.noreply.github.com>
2022-08-14 20:29:39 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int uninitializedVariable;
+		System.out.println(uninitializedVariable);
+		
+		String 5numbers = "12345";
+		
+		double number = 10;
+		String number = "10";
+		
+		float &someVariable = 876;
+		
+	}
+	
+}
